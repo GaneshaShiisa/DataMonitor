ae8577a719b2dc43880a2c0f9e8f1d9d05af0ecc
Ganesha Shiisa <Ganesha.Shiisa@gmail.com>
2022-09-07 17:18:13 +0900

    Add: 複数軸プロット機能追加

--- data_monitor.py
+++ data_monitor.py
@@ -14,12 +14,13 @@
 legendの表示
 """
 import tkinter
-from tkinter import ttk
+from tkinter import W, ttk
 import tkinter.font
 
 import os
 import time
 import hashlib
+from turtle import pos
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
@@ -50,44 +51,34 @@ class DataMonitor(tkinter.Frame):
         # ウィジェットの配置
         self.create_widgets()
 
-        # 初期値の設定
-        # self.base_time = time.time()
-        # self.pause_time = 0
-        # self.pause_time_buf = 0
-        # self.status = self.STATUS_STOP
-        # self.flash_count = 0
-        # self.previous_position = pyautogui.position()
-        # self.active_time = time.time()
-
-        # スケジューラの起動
-        # self.time_buf = round(time.time(), 0)
-        # self.time_event()
-
         # ファイル更新管理準備
         self.observer = Observer()
 
         # 設定ファイル読み込み
-
         if os.path.exists(self.setting_file):
             with open(self.setting_file, 'r') as fp:
                 setting_data = json.load(fp)
                 self.file_path = setting_data["file_path"]
                 self.data_name = setting_data["data_name"]
                 self.data_setting = setting_data["data_setting"]
-
-                self.open_file(self.file_path)
+                geometry = setting_data["geometry"]
         else:
             self.file_path = ""
             self.data_name = ""
             self.data_setting = ""
+            geometry = str(1000)+"x"+str(500) + "+" + \
+                str(master.winfo_screenwidth()-1000-10)+"+0"
 
+        root.geometry(geometry)
+
+        self.open_file(self.file_path)
         #
         self.master.protocol("WM_DELETE_WINDOW", self.delete_window)
 
     def delete_window(self):
         print("終了")
         data_monitor_setting = {
-            "file_path": self.file_path, "data_name": self.data_name, "data_setting": self.data_setting}
+            "file_path": self.file_path, "data_name": self.data_name, "data_setting": self.data_setting, "geometry": self.master.geometry()}
 
         with open(self.setting_file, 'w') as fp:
             json.dump(data_monitor_setting, fp, indent=4)
@@ -105,91 +96,17 @@ class DataMonitor(tkinter.Frame):
         frame = tkinter.Frame(self)
 
         # matplotlibの描画領域の作成
-        fig = Figure(figsize=(self.width/100, self.height/100))
+        self.fig = Figure(figsize=(self.width/100, self.height/100))
 
-        fig.canvas.mpl_connect('button_press_event', self.onclick)
-        fig.canvas.mpl_connect('motion_notify_event', self.mouse_move)
-
-        # 座標軸の作成
-        self.ax = fig.add_subplot(1, 1, 1)
+        # fig.canvas.mpl_connect('button_press_event', self.onclick)
+        # fig.canvas.mpl_connect('motion_notify_event', self.mouse_move)
 
         # matplotlibの描画領域とウィジェット(Frame)の関連付け
-        self.fig_canvas = FigureCanvasTkAgg(fig, frame)
-        # matplotlibのツールバーを作成
-        # self.toolbar = NavigationToolbar2Tk(self.fig_canvas, frame)
+        self.fig_canvas = FigureCanvasTkAgg(self.fig, frame)
         # matplotlibのグラフをフレームに配置
         self.fig_canvas.get_tk_widget().pack(fill=tkinter.BOTH, expand=True)
 
         frame.pack()
-
-        # # データ読み込み
-        # data = pd.read_csv(
-        #     R'data.csv', header=0, index_col=0)
-        # data_name = self.data.columns.values
-
-        # # data[data_name[2]].plot()
-
-        # self.ax.plot(data['Time']/1e6, data[data_name[2]])
-        # self.ax.grid()
-        # self.ax.set_ylabel(data_name[2])
-        # self.fig_canvas.draw()
-
-        # self.label_window_size = ttk.Label(
-        #     self, text='')
-        # self.label_window_size.grid(row=0, column=0)
-        # self.label_window_size.pack(side=tkinter.BOTTOM, anchor=tkinter.W)
-        # self.label_window_size.place(x=10, y=10, width=50, height=50)
-        # self.label_colon = tkinter.Label(self, text=':', font=font_timer)
-        # self.label_colon.grid(row=0, column=1, rowspan=2)
-        # self.label_ss = tkinter.Label(
-        #     self, text='{:02}'.format(0), font=font_timer, width=2)
-        # self.label_ss.grid(row=0, column=2, rowspan=2)
-
-        # self.blank = tkinter.Label(self, text=' ')
-        # self.blank.grid(row=0, column=3, rowspan=2)
-
-        # font_button = tkinter.font.Font(family="游ゴシック", size=12)
-        # self.button_work_start = tkinter.Button(
-        #     self, text="作業 ▶", font=font_button, width=6,
-        #     command=self.evt_button_work_start)
-
-        # self.button_work_start.grid(row=0, column=4)
-        # self.button_break_start = tkinter.Button(
-        #     self, text="休憩 ▶", font=font_button, width=6,
-        #     command=self.evt_button_break_start)
-        # self.button_break_start.grid(row=1, column=4)
-        # self.button_pause = tkinter.Button(
-        #     self, text="一時停止", font=font_button, width=6,
-        #     command=self.evt_button_pause)
-        # self.button_pause.grid(row=0, column=5)
-        # self.button_stop = tkinter.Button(
-        #     self, text="停止", font=font_button, width=6,
-        #     command=self.evt_button_stop)
-        # self.button_stop.grid(row=1, column=5)
-
-    def set_background_colour(self, color_name):
-        """背景色を設定する
-
-        Args:
-            color_name (int): 色名称
-        """
-        # self['bg'] = color_name
-        # self.label_mm['bg'] = color_name
-        # self.label_colon['bg'] = color_name
-        # self.label_ss['bg'] = color_name
-        # self.blank['bg'] = color_name
-
-    def set_window_active(self):
-        """Windowのアクティブ化
-        """
-        # # Windowを最前面に移動する
-        # self.master.attributes("-topmost", True)
-        # self.master.attributes("-topmost", False)
-        # # Windowをアクティブにする
-        # x, y = pyautogui.position()
-        # pyautogui.moveTo(self.master.winfo_x(), self.master.winfo_y())
-        # pyautogui.click()
-        # pyautogui.moveTo(x, y)  # 元の位置にマウスを戻す
 
     def onclick(self, event):
         print('button=%d, x=%d, y=%d' % (
@@ -209,7 +126,6 @@ class DataMonitor(tkinter.Frame):
         self.fig_canvas.draw()
 
     def open_file(self, file_path):
-
         # データ読み込み
         if self.read_data(file_path):
             # ファイル更新監視
@@ -253,21 +169,9 @@ class DataMonitor(tkinter.Frame):
             self.data_name = data_name
             self.data_setting = {}
             for name in self.data_name:
-                self.data_setting[name] = dict(enable=False, test=100)
+                self.data_setting[name] = dict(enable=False, position="1")
 
         return True
-
-    def plot_data(self):
-        self.ax.cla()
-        for name in self.data_name:
-            if self.data_setting[name]['enable']:
-                self.ax.plot(self.data['Time']/1e6,
-                             self.data[name], label=name)
-        self.ax.grid()
-        self.ax.legend()
-        xmin, xmax = self.ax.get_xlim()
-        self.ax_ln = self.ax.axvline(xmin)
-        self.fig_canvas.draw()
 
     def on_modified(self, event):
         file_path = event.src_path
@@ -289,8 +193,7 @@ class DataMonitor(tkinter.Frame):
             return hashlib.md5(f.read()).hexdigest()
 
     def command_open_file(self):
-        file_path = tkinter.filedialog.askopenfilename(
-            initialdir=R'C:\Users\kota\Documents\Python\DataMonitor')
+        file_path = tkinter.filedialog.askopenfilename()
         self.open_file(file_path)
 
     def command_data_select(self):
@@ -299,19 +202,25 @@ class DataMonitor(tkinter.Frame):
                      str(self.master.winfo_y()+self.master.winfo_height()))
 
         self.data_enable = []
+        self.data_position = []
         for i in range(len(self.data_name)):
             # チェックボックスの設置
             bln = tkinter.BooleanVar()
             bln.set(self.data_setting[self.data_name[i]]['enable'])
             c = tkinter.Checkbutton(
-                win, variable=bln, width=5, text='', background='white')
+                win, variable=bln, width=0, text='', background='white')
             self.data_enable.append(bln)  # チェックボックスの初期値
             c.grid(row=i, column=0, padx=0, pady=0, ipadx=0, ipady=0)  # 0列目
+            # グラフポジション
+            cb = ttk.Combobox(win, values=[1, 2, 3, 4], width=1)
+            cb.set(self.data_setting[self.data_name[i]]['position'])
+            self.data_position.append(cb)
+            cb.grid(row=i, column=1, padx=0, pady=0, ipadx=0, ipady=0)  # 1列目
             # データ名
             a1 = self.data_name[i]
             b1 = tkinter.Label(win, width=20, text=a1,
                                background='white', anchor='w')
-            b1.grid(row=i, column=1, padx=0, pady=0, ipadx=0, ipady=0)  # 1列目
+            b1.grid(row=i, column=2, padx=0, pady=0, ipadx=0, ipady=0)  # 2列目
         plotButton = tkinter.Button(
             win, text='make plot', command=self.make_plot)
         plotButton.grid(row=len(self.data_name)+1, column=3)  # 3列目
@@ -320,13 +229,26 @@ class DataMonitor(tkinter.Frame):
         for i in range(len(self.data_name)):
             self.data_setting[self.data_name[i]
                               ]['enable'] = self.data_enable[i].get()
-        self.ax.cla()
+            self.data_setting[self.data_name[i]
+                              ]['position'] = self.data_position[i].get()
+        # データ描画
+        self.plot_data()
+
+    def plot_data(self):
+        self.fig.clear()
+        # 座標軸の作成
+        positions = []
         for name in self.data_name:
-            if self.data_setting[name]['enable']:
-                self.ax.plot(self.data['Time']/1e6,
-                             self.data[name])
-        self.ax.grid()
-        self.ax.set_ylabel("pitch")  # self.data_name[2]
+            positions.append(int(self.data_setting[name]["position"]))
+        subplot_num = max(positions)
+        for i in range(subplot_num):
+            ax = self.fig.add_subplot(subplot_num, 1, 1+i)
+            for name in self.data_name:
+                if self.data_setting[name]['enable'] and int(self.data_setting[name]['position']) == (i+1):
+                    ax.plot(self.data['Time']/1e6,
+                            self.data[name], label=name)
+            ax.grid()
+            ax.legend()
         self.fig_canvas.draw()
 
 
@@ -365,11 +287,11 @@ if __name__ == "__main__":
     root = tkinter.Tk()
     # Windowsサイズの設定
     root.title("Data Monitor")
-    width = 1000
-    height = 500
-    screen_width = root.winfo_screenwidth()
-    root.geometry(str(width)+"x"+str(height) +
-                  "+"+str(screen_width-width-10)+"+0")
+    # width = 1000
+    # height = 500
+    # screen_width = root.winfo_screenwidth()
+    # root.geometry(str(width)+"x"+str(height) +
+    #               "+"+str(screen_width-width-10)+"+0")
     # master.resizable(0, 0)
 
     status_bar = StatusBar(master=root)
